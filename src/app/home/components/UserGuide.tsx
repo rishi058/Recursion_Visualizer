5493efbd46658ff791313ef52c2e8a6bcb7bf2d8
Rishi <rishi.helloworld@gmail.com>
2024-07-03 23:15:58 +0530

    Improved UI, boiler plate, example

--- src/app/home/components/UserGuide.tsx
+++ src/app/home/components/UserGuide.tsx
@@ -10,7 +10,8 @@ function UserGuide() {
           </p>
         </div>
       </div>
-      <div className="px-20 lg:px-40  pt-16 text-xl font-medium  bg-clip-text text-transparent bg-gradient-to-r from-gray-400 to-black">
+      <div className="mx-15 lg:mx-40 font-medium mt-16 p-5 text-xl text-gray-400 bg-gradient-to-r from-gray-900 to-gray-700">
+        <br />
         <p>
           <span className="font-semibold text-gray-400">Step 0 :</span>
           <br /> Paste your recursive code into the IDE below. Make the
@@ -18,7 +19,8 @@ function UserGuide() {
         </p>
         <br />
         <p className="italic">
-          Note: Do not modify any of the already written code. If any of your
+          <span className="font-bold">Note: </span> 
+          Do not modify any of the already written code. If any of your
           variable names collide with the boilerplate, please modify your code
           accordingly.
         </p>
@@ -32,6 +34,7 @@ function UserGuide() {
           Just after entering the recursive function, create a string named node
           which includes the function name and the values of the parameters you
           want to see in the node.
+          <br /> <br />
           Then Insert this :{" "}
           <span className="bg-slate-500 text-white">
             int currentId = addNode(node); addEdge(par,currentId);
@@ -41,7 +44,7 @@ function UserGuide() {
         </p>
         <br />
         <p className="italic">
-          Note: Do not make the size of the string node very large, as it will
+        <span className="font-bold">Note:</span> Do not make the size of the string node very large, as it will
           affect the appearance of the tree. Insert large intermediate values by
           following Step 2. This data can be viewed by hovering over the node.
         </p>
@@ -55,19 +58,21 @@ function UserGuide() {
           <span className="bg-slate-500 text-white">
             nodeToAns[currentId] = to_string(return_value);
           </span>
+          <br /> <br />
+          At last run the code by giving input(if any), a output will be generated after that click on Visualize button.
           <br />
           <br />
           <span className="italic">
             <span className="font-bold">Note : </span><br />
             1. Right now, only C++ is supported. I will soon add support for other languages.
-            <br />
+            <br /> <br />
             2. The visualization can be a slow process because at each step we are using to_string() function.
-            <br />
-            3. If the online IDE is not wroking, copy the boiler-plate code. Paste the code in any other IDE & modify the code.
-            Generated the output and paste in output field here and Visualize. <br />
-            4. Consider increasing the time-limit & stack-limit of compiler you are using if output is not generated. <br />
-            5. Dont add &quot;/n&quot; when coverting data to string. It will break the JSON. You can only use &quot;//n&quot; or the defined macro br(in C++) for a new line in strings. <br />
-            6. You can use pre-defined functions like getVectorToString, get2DVectorToString to convert vectors to string. <br />
+            <br /> <br />
+            3. If the online IDE is not wroking, copy the boiler-plate in any other IDE & modify the code.
+            Generate and paste the output here and click on Visualize. <br /> <br />
+            4. Consider increasing the time-limit & stack-limit of compiler you are using if output is not generated. <br /> <br />
+            5. Dont add &quot;/n&quot; when coverting data to string. It will break the JSON. You can only use &quot;//n&quot; or the defined macro br(in C++) for a new line in strings. <br /> <br />
+            6. You can use pre-defined functions like getVectorToString, get2DVectorToString to convert vectors to string. <br /> <br />
           </span>
         </p>
       </div>
